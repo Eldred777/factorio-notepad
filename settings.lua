0041c105c59fcfdac8feece8b6b929e9200ec5be
Ciaran KG <55831661+Eldred777@users.noreply.github.com>
2024-07-14 11:56:58 +1000

    Renamed mod to notepad

--- settings.lua
+++ settings.lua
@@ -1,8 +1,8 @@
 data:extend { {
   type = "bool-setting",
-  name = "eld-note-debug",
+  name = "notepad-debug",
   setting_type = "runtime-global",
   default_value = false,
-  hidden = not mods["sandbox-tweaks"] and true or false,
+  -- hidden = not mods["sandbox-tweaks"] and true or false,
   localised_name = "Debug",
 } }
